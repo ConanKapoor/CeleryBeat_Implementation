7bde42307d5628a4b6da8f0e565bbb1e075e0348
ConanKapoor <kapoor.shivam88@gmail.com>
2017-05-24 15:47:37 +0530

    sahi kar diya output file

--- testing/views.py
+++ testing/views.py
@@ -5,13 +5,12 @@ from django.shortcuts import render
 import os, subprocess, sys
 
 # Create your views here.
-
 def url_retrieval(request):
     if request.method == 'POST':
         web_url = request.POST.get('user')
-        b = subprocess.check_output("nslookup %s" %web_url, shell = TRUE)
+
+        b = subprocess.check_output("nslookup %s" %web_url, shell = True)
         f = open('ouput_of_request', 'w')
         f.write(b)
         f.close()
-
     return render(request, 'testing/userdata.html')
